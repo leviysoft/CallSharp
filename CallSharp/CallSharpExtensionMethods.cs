4bc9f07e91000bfe0fc4afa32ec88242168149f1
Daniil Smirnov <danslapman@gmail.com>
2015-07-14 16:36:51 +0300

    Add nuspec and license. Add doc comments.

--- CallSharp/CallSharpExtensionMethods.cs
+++ CallSharp/CallSharpExtensionMethods.cs
@@ -2,13 +2,32 @@
 
 namespace CallSharp
 {
+    /// <summary>
+    /// Generic extension methods for monadic exception handling
+    /// </summary>
     public static class CallSharpExtensionMethods
     {
+        /// <summary>
+        /// Wraps a function call in Func&lt;&gt;
+        /// </summary>
+        /// <param name="instance">Object, which method must be called</param>
+        /// <param name="call">Method call function</param>
+        /// <typeparam name="T">Type of the object</typeparam>
+        /// <typeparam name="TRes">Result type</typeparam>
+        /// <returns>Function that calls the method of the object and returns the result</returns>
         public static Func<TRes> Call<T, TRes>(this T instance, Func<T, TRes> call)
         {
             return () => call(instance);
         }
 
+        /// <summary>
+        /// Wraps a function call in a try/catch block
+        /// </summary>
+        /// <param name="func">A function to call</param>
+        /// <param name="convertor">A function that computes some value based on exception instance</param>
+        /// <typeparam name="TRes">Type of the result</typeparam>
+        /// <typeparam name="TException">Type of the exception</typeparam>
+        /// <returns>Some value</returns>
         public static Func<TRes> Catch<TRes, TException>(this Func<TRes> func, Func<TException, TRes> convertor)
             where TException : Exception
         {
--- CallSharp/Properties/AssemblyInfo.cs
+++ CallSharp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CallSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Monadic exception handler for C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("LeviySoft")]
 [assembly: AssemblyProduct("CallSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © danslapman 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
